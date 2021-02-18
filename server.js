aaa2c22b587f3c1c6b7b0c4f6ca22fed5b599d36
Stacy Betts <stacyo23@yahoo.com>
2021-02-17 18:28:19 -0600

    attempting to fix crashed app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI  || "mongodb://localhost/budget", {
+mongoose.connect(process.env.dbURI || "mongodb://localhost/budget", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
