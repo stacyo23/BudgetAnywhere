d8f770bf0a329edbb034cc3693fd7c1d197342d8
Stacy Betts <stacyo23@yahoo.com>
2021-02-17 18:25:19 -0600

    saving to deploy

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(MONGODB_URI  || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
